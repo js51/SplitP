3be9125190cd749f24da18e0c61ee87fb339e01e
Joshua Stevenson <joshuas153@Gmail.com>
2021-04-16 13:00:13 +1000

    restructure enums

--- splitp/__init__.py
+++ splitp/__init__.py
@@ -3,4 +3,5 @@ from splitp.nx_tree import *
 from splitp.parsers import *
 from splitp.tree_helper_functions import *
 from splitp.squangles import *
-from splitp.models import *
+from splitp.enums import *
+from splitp.tree_reconstruction import *
