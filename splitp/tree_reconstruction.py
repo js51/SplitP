a459263fe236035bebbe715576c98aaa5ee797fe
Joshua Stevenson <joshuas153@Gmail.com>
2021-04-19 12:15:48 +1000

    add new tree reconstruction algorithm

--- splitp/tree_reconstruction.py
+++ splitp/tree_reconstruction.py
@@ -6,6 +6,7 @@
 
 import splitp as sp
 from splitp import tree_helper_functions as hf
+import numpy as np
 
 def erickson_SVD():
     pass
@@ -28,4 +29,25 @@ def split_tree_parsimony(alignment):
         split_tree = sp.NXTree(newick_string, taxa_ordering='sorted')
         for pattern, value in alignment_dict.items():
             scores[split] += value * (split_tree.hartigan_algorithm(pattern)/(num_taxa-1))
+    return scores
+
+def normalised_euclidean_distance(alignment):
+    print("assuming sorted taxa")
+    states = ('A', 'G', 'C', 'T')
+    alignment_dict = {}
+    for table_pattern, value in alignment.itertuples(index=False, name=None):
+        alignment_dict[table_pattern] = value
+    num_taxa = len(list(alignment_dict.keys())[0]) # Length of first pattern
+    all_splits = list(hf.all_splits(num_taxa))
+    scores = {split : 0 for split in all_splits}
+    for split in all_splits:
+        split = split.split('|')
+        for pattern, value in alignment_dict.items():
+            part_a = "".join(pattern[int(s)] for s in split[0])
+            part_b = "".join(pattern[int(s)] for s in split[1])
+            vec_a = np.array(( part_a.count(state) for state in states ))
+            vec_b = np.array(( part_b.count(state) for state in states ))
+            vec_a = vec_a / np.linalg.norm(vec_a)
+            vec_b = vec_b / np.linalg.norm(vec_b)
+            scores[split] += value * (2-np.linalg.norm(vec_a - vec_b))
     return scores
