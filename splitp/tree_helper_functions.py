5de1ee55cf1e7e95641cae19f0242c347c65b4a6
Joshua Stevenson <joshuas153@Gmail.com>
2021-04-20 13:59:54 +1000

    fix last commit

--- splitp/tree_helper_functions.py
+++ splitp/tree_helper_functions.py
@@ -177,7 +177,7 @@ def normalised(scores):
     if type(scores) is dict:
         minimum = min(scores.values())
         maximum = max(scores.values())
-        for key, value in scores.values():
+        for key, value in scores.items():
             scores[key] = (value - minimum)/(maximum - minimum)
     elif type(scores) is list:
         minimum = min(scores)
