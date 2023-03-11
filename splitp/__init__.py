69165c4a776da83642fbe576d52686fd23c72d5a
Joshua Stevenson <mail@joshuastevenson.me>
2023-03-11 08:16:37 +0000

    Remove rust bindings (for now!)

--- splitp/__init__.py
+++ splitp/__init__.py
@@ -1,18 +1,6 @@
 # Python imports
-from ._setuptools_rust_starter import __all__, __doc__
-from ._setuptools_rust_starter import *
 from splitp.phylogeny import *
 from splitp.enums import *
 from splitp import alignment
 from splitp import constants
 from splitp import constructions
-
-name = "splitp"
-
-# Rust bindings
-__all__ = __all__ + ["PythonClass"]
-
-
-class PythonClass:
-    def __init__(self, value: int) -> None:
-        self.value = value
